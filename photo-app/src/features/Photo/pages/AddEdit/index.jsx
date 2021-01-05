2ee5c304e5021d14bf3bc3d423c4bfaac86fb6b9
Henry Nguyen <huannguyen4404@gmail.com>
2021-01-05 13:43:31 +0700

    setup routes and lazy loading components

---
+++ photo-app/src/features/Photo/pages/AddEdit/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './styles.scss'
+
+AddEditPage.propTypes = {}
+
+function AddEditPage(props) {
+  return <div className="photo-edit">Edit photo page</div>
+}
+
+export default AddEditPage
---
+++ photo-app/src/features/Photo/pages/Main/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+MainPage.propTypes = {}
+
+function MainPage(props) {
+  return <div>Main Photo Page</div>
+}
+
+export default MainPage
